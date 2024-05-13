84d1c795cc0b3824e46cf72dd0fd47753cd48a1d
GastonRomero269 <114840130+GastonRomero269@users.noreply.github.com>
2024-05-12 23:58:09 -0300

    A

---
+++ src/test/java/com/grupo_A/TP/TpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo_A.TP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
